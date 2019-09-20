c8df90f74b3e1bb166b04eb5595670e80161d7a7
rE97 <mhafiansyah@gmail.com>
2019-09-21 06:14:48 +0700

    Remove the functional javascript workshop folder

--- functional javascript workshop/basicFilter/index.js
+++
@@ -1,7 +0,0 @@
-function getShortMessages(messages) {
-	return filtered = messages.filter(value => {
-		return value.message.length < 50;
-	}).map(value => value.message);
-}
-
-module.exports = getShortMessages;
--- functional javascript workshop/basicMap/index.js
+++
@@ -1,9 +0,0 @@
-function doubleAll(numbers) {
-	let results = numbers.map(value => {
-		return value * 2
-	});
-
-	return results;
-}
-
-module.exports = doubleAll;
