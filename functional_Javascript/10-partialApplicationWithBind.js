734cae105d574286f6c5080d0685a9dee7e2dbaf
rE97 <mhafiansyah@gmail.com>
2019-09-30 06:07:30 +0700

    add more explaination for solution 10

--- functional_Javascript/10-partialApplicationWithBind.js
+++ functional_Javascript/10-partialApplicationWithBind.js
@@ -2,3 +2,11 @@
 module.exports = function(namespace) {
 	return console.log.bind(console, namespace)
 }
+
+// https://stackoverflow.com/a/28668819, for better understanding
+
+
+// if you confused how the function will print the argument after called, look the following code
+// let cl = console.log.bind(console)
+// cl('argument here')
+// the 'argument here' will be printed, even if console.log already called
